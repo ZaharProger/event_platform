ddcf98439db3d7ae339662be6f25963ffcbf2260
Zahar Domolego <domolegozahar@gmail.com>
2024-03-12 11:32:19 +0800

    Added user group view

--- event_platform/users/urls.py
+++ event_platform/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LoginView, LogoutView, AccountDataView
+from .views import LoginView, LogoutView, AccountDataView, UserGroupsView
 
 urlpatterns = [
     path('account/', AccountDataView.as_view(), name='account'),
     path('auth/', LoginView.as_view(), name='login'),
+    path('groups/', UserGroupsView.as_view(), name='groups'),
     path('logout/', LogoutView.as_view(), name='logout')
 ]
--- event_platform/users/views.py
+++ event_platform/users/views.py
@@ -1,14 +1,15 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from django.db.transaction import atomic
 from django.contrib.auth import login, logout
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
-from .models import UserProfile, UserPassport
+from .models import UserPassport
 from .forms import UserPassportForm
 from .serializers import UserPassportSerializer, UserProfileSerializer
+
+import os
 
 
 class LoginView(APIView):
@@ -22,15 +23,18 @@ class LoginView(APIView):
                 login(request, found_passport[0])
                 message = ''
                 response_status = status.HTTP_200_OK
+                is_superuser = found_passport[0].is_superuser
             else:
                 message = 'Введен неверный пароль!'
                 response_status = status.HTTP_400_BAD_REQUEST
+                is_superuser = False
         else:
             message = 'Пользователя с указанным логином не существует!'
             response_status = status.HTTP_400_BAD_REQUEST
+            is_superuser = False
             
         return Response(
-            {'message': message}, 
+            {'message': message, 'is_superuser': is_superuser}, 
             status=response_status,
             content_type='application/json'
         )
@@ -51,9 +55,15 @@ class AccountDataView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
+        # user_profile = UserProfile.objects.create(
+        #     name='',
+        #     email=''
+        # )
+        # user_profile.save()
         # new_user = UserPassport()
+        # new_user.is_superuser = True
         # new_user.username = ''
-        # new_user.user = UserProfile.objects.all()[2]
+        # new_user.user = user_profile
         # new_user.set_password('')
         # new_user.save()
         
@@ -83,3 +93,63 @@ class AccountDataView(APIView):
             status=response_status,
             content_type='application/json'
         ) 
+    
+
+class UserGroupsView(APIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        templates_path = os.path.join('event_platform', 'static')
+        odd_dir_list = ['export', '.DS_Store']
+        data = [{'name': dir} for dir in os.listdir(templates_path) if dir not in odd_dir_list]
+
+        return Response(
+            {'data': data},
+            status=status.HTTP_200_OK,
+            content_type='application/json'
+        ) 
+
+    def post(self, request):
+        found_passport = UserPassport.objects.filter(username=request.user.username)
+        if len(found_passport) != 0:
+            found_passport[0].doc_template = request.data['name']
+            found_passport[0].save()
+            docs_path = os.path.join('event_platform', 'static', request.data['name'])
+            os.mkdir(docs_path)
+
+        return Response(
+            {'message': ''},
+            status=status.HTTP_200_OK,
+            content_type='application/json'
+        ) 
+
+    def put(self, request):
+        found_passport = UserPassport.objects.filter(username=request.user.username)
+        if len(found_passport) != 0:
+            pass
+
+        return Response(
+            {'message': ''},
+            status=status.HTTP_200_OK,
+            content_type='application/json'
+        )
+
+    def delete(self, request):
+        found_passport = UserPassport.objects.filter(username=request.user.username)
+        group_to_delete = request.GET.get('name', None)
+
+        if len(found_passport) != 0 and group_to_delete is not None:
+            passports_to_update = UserPassport.objects.filter(doc_template=group_to_delete)
+            for passport in passports_to_update:
+                passport.doc_template = ''
+                passport.save()
+
+            os.rmdir(os.path.join('event_platform', 'static', group_to_delete))
+
+        return Response(
+            {'message': ''},
+            status=status.HTTP_200_OK,
+            content_type='application/json'
+        )
+    
